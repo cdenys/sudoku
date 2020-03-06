45dd924026d8f1eb07a780cc02145f251fd0bc60
Patrice Cadiot <pathibul.r@gmail.com>
2020-03-06 16:26:34 +0100

    add another test for the forth region

--- sudoku_test.rb
+++ sudoku_test.rb
@@ -67,6 +67,21 @@ class SudokuTest < Minitest::Test
     assert_equal expected, Sudoku.new(grid).regions[2].flatten
   end
 
+  def test_it_finds_the_forth_region
+    # skip
+    grid = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
+            [6, 7, 2, 1, 9, 5, 3, 4, 8],
+            [1, 9, 8, 3, 4, 2, 5, 6, 7],
+            [8, 5, 9, 7, 6, 1, 4, 2, 3],
+            [4, 2, 6, 8, 5, 3, 7, 9, 1],
+            [7, 1, 3, 9, 2, 4, 8, 5, 6],
+            [9, 6, 1, 5, 3, 7, 2, 8, 4],
+            [2, 8, 7, 4, 1, 9, 6, 3, 5],
+            [3, 4, 5, 2, 8, 6, 1, 7, 9]]
+    expected = [8, 5, 9, 4, 2, 6,7, 1, 3]
+    assert_equal expected, Sudoku.new(grid).regions[3].flatten
+  end
+
   def test_row_is_valid
     # skip
     grid = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
